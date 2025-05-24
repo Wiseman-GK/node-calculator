a7962fbab1ec1eb214148271f11e5638356bf439
Wiseman-GK <Wiseman2xGK@gmail.com>
2025-05-24 16:43:10 -0700

    fixed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-var rs = require('readline-sync');
+const rs = require('readline-sync');
+const operator = ['+', '-', '*', '/'];
+function operation(){
+  let numOne = rs.questionInt('Enter The First Number ');
+  console.log(numOne);
+  
+  let opInput = rs.question('Enter An Operator ');
+  console.log(numOne);
+  if(operator.includes(opInput)){
+    console.log(opInput);
+  } else{
+    console.log('Invalid Operator ');
+    operation();
+  }
+
+  let numTwo = rs.questionInt('Enter The Second Number ');
+  console.log(numTwo);
+
+  if(opInput === '+'){
+    console.log('Result: ', Number(numOne) + Number(numTwo));
+  }
+  if(opInput === '-'){
+    console.log('Result: ', Number(numOne) - Number(numTwo));
+  }
+  if(opInput === '*'){
+    console.log('Result: ', Number(numOne) * Number(numTwo));
+  }
+  if(opInput === '/'){
+    if(numTwo === 0){
+      console.log('Cannot Divide By Zero');
+      operation()
+    } else {
+      console.log('Result: ', Number(numOne) / Number(numTwo));
+    }
+  }
+}
+operation();
